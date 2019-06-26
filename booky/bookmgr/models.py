62ca292f026062f400e1251cbfe1d598043905b3
karmek-k <33288445+karmek-k@users.noreply.github.com>
2019-06-26 16:28:52 +0200

    Register Book model

--- booky/bookmgr/models.py
+++ booky/bookmgr/models.py
@@ -5,3 +5,6 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     has_been_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
