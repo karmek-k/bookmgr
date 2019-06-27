45b1ce4ebac3b187d11f66a39df80a574b4801d0
karmek-k <33288445+karmek-k@users.noreply.github.com>
2019-06-27 12:32:32 +0200

    Increase Book.title max_length

--- booky/bookmgr/models.py
+++ booky/bookmgr/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
     description = models.TextField()
     has_been_read = models.BooleanField(default=False)
 
